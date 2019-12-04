3e1d2f456b45f710ec7a2c62b37bf215b3a18fc6
Sophie Torresan <sophie.torresan@free.fr>
2019-12-04 13:10:15 +0100

    ajout se connecter

--- view/frontend/loginView.php
+++ view/frontend/loginView.php
@@ -8,18 +8,19 @@
     <div class="container">
         <div class="row" >
             <div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3 login-content pb-5 px-5">
-                <form id="form-login" action="index.php?action=login" method="post" novalidate="novalidate">
+                <form id="form-login" action="index.php?action=login" method="post">
+
                     <div class="form-group mt-5">
-                       <!-- <?php if($error) { ?>
+                       <?php if($error) { ?>
                             <p class="ext-center text-danger mt-3">Mauvais identifiant ou mot de passe. Merci de réessayer.</p>
-                        <?php } ?>-->
+                        <?php } ?>
 
                         <label for="pseudo" >Votre identifiant</label><br>
                         <input type="text" value="" class="form-control" name="pseudo" id="pseudo" placeholder="Veuillez saisir votre identifiant" required="">
                     </div>
                     <div class="form-group">
                         <label for="password" >Mot de passe</label>
-                        <input type="password" value="" class="form-control" name="password" id="password" placeholder="Veuillez saisir votre mot de passe" required="">
+                        <input type="password" value="" class="form-control" name="pass" id="pass" placeholder="Veuillez saisir votre mot de passe" required="">
                     </div>
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary mt-3 float-none float-md-right">Se connecter</button>
@@ -32,5 +33,7 @@
 
 
 <?php $content = ob_get_clean(); ?>
+
+
 
 <?php require('template.php'); ?>
