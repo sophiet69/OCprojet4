ef8baf9ff9995aa29529b2fb27aa7efee84e482b
sophiet69 <53080118+sophiet69@users.noreply.github.com>
2019-12-07 07:36:17 +0100

    ajout de report a getComment

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -19,7 +19,7 @@ class CommentManager extends Manager
     public function getComments($postId)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments = $db->prepare('SELECT id, author, comment,report DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
         $comments->execute(array($postId));
 
         return $comments;
