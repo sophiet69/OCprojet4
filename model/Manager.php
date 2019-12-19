eaf401661d9c2a7d8970f4d17a596e0b06d02a38
sophiet69 <53080118+sophiet69@users.noreply.github.com>
2019-12-19 15:43:25 +0100

    activation des erreurs PDO

--- model/Manager.php
+++ model/Manager.php
@@ -7,6 +7,8 @@ class Manager
     protected function dbConnect()
     {
         $db = new \PDO('mysql:host=localhost;dbname=blog-ecrivain;charset=utf8', 'root', '');
+        // Activation des erreurs PDO
+        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         return $db;
     }
 }
